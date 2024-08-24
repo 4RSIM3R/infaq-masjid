e96aa633f652de7deb8fb1e55e23cbd65a87117c
ilzam <ilzammulkhaq85@gmail.com>
2024-08-25 06:38:10 +0700

    fix: campaign url

--- routes/web/campaign.php
+++ routes/web/campaign.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'backoffice/campaign', 'as' => 'campaign.'], function () {
     Route::resource('', CampaignController::class)->except(['destroy', 'show']);
     Route::delete('{id}/delete', [CampaignController::class, 'destroy'])->name('destroy');
-    Route::get('{id}', [CampaignController::class, 'show'])->name('show');
+    Route::get('{id}/detail', [CampaignController::class, 'show'])->name('show');
 });
