be6f18ed980bf4954fc525a6299d06c54053b884
ilzam <ilzammulkhaq85@gmail.com>
2024-08-25 06:37:53 +0700

    fix: campaign utl

--- routes/web/campaign.php
+++ routes/web/campaign.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\CampaignController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'backoffice/campaign', 'as' => 'campaign.'], function () {
+    Route::resource('', CampaignController::class)->except(['destroy', 'show']);
     Route::delete('{id}/delete', [CampaignController::class, 'destroy'])->name('destroy');
     Route::get('{id}', [CampaignController::class, 'show'])->name('show');
-    Route::resource('', CampaignController::class)->except(['destroy', 'show']);
 });
