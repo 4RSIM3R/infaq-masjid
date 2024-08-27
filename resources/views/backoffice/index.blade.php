249586e0ccea14220961eec322e1b4292fbd7edc
ilzam <ilzammulkhaq85@gmail.com>
2024-08-27 11:33:47 +0700

    feat: mari

--- resources/views/backoffice/index.blade.php
+++ resources/views/backoffice/index.blade.php
@@ -48,9 +48,6 @@
                     <th scope="col" class="px-6 py-3">
                         Progres
                     </th>
-                    <th scope="col" class="px-6 py-3">
-                        Action
-                    </th>
                 </tr>
             </thead>
             <tbody>
