c54b52edafc737dd548e9e98583a8c21edd7088d
ilzam <ilzammulkhaq85@gmail.com>
2024-09-03 21:46:07 +0700

    fix: delete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,13 @@
 
 @section('content')
     <div class="relative isolate overflow-hidden">
+        <!-- Background Image -->
         <img src="{{ asset('masjid.jpg') }}" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
+
+        <!-- Black Shadow Overlay -->
+        <div class="absolute inset-0 bg-black opacity-50 -z-10"></div>
+
+        <!-- Main Content -->
         <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
             <div class="hidden sm:mb-8 sm:flex sm:justify-center">
                 <div
@@ -26,16 +32,14 @@
                 </div>
             </div>
         </div>
-        <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-            aria-hidden="true">
-            <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-                style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
-            </div>
-        </div>
+
+        <!-- Decorative Background Element -->
     </div>
+
     <div class="my-16 max-w-screen-xl mx-auto flex flex-col">
         <p class="text-2xl font-bold text-center">Yuk Ber-infaq</p>
-        <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 place-items-center justify-items-center">
+        <div
+            class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 place-items-center justify-items-center">
             @foreach ($campaigns as $data)
                 <div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
                     <a href="{{ route('home.campaign_detail', $data->id) }}">
--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -4,20 +4,15 @@
     <main class="isolate">
         <!-- Hero section -->
         <div class="mx-auto max-w-7xl px-6 py-24">
-            <div
-                class="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
-                <h1 class="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
-                    BerInfaq
-                </h1>
-                <div class="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
-                    <p class="text-lg leading-8 text-gray-600">
-                        BerInfaq adalah aplikasi manajemen infaq masjid yang dirancang untuk memudahkan pengelolaan donasi
-                        secara transparan dan efisien. Aplikasi ini memungkinkan jamaah masjid untuk memberikan infaq secara
-                        digital melalui berbagai metode pembayaran, seperti transfer bank, e-wallet, atau QR code.
-                    </p>
-                </div>
-                <img src="https://baznas.go.id/assets/images/baznas-infaq-infak.jpg" alt=""
-                    class="aspect-[6/5] w-full max-w-lg rounded-2xl object-cover lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2">
+            <h1 class="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
+                BerInfaq
+            </h1>
+            <div class="mt-8 max-w-xl xl:col-end-1 xl:row-start-1">
+                <p class="text-lg leading-8 text-gray-600">
+                    BerInfaq adalah aplikasi manajemen infaq masjid yang dirancang untuk memudahkan pengelolaan donasi
+                    secara transparan dan efisien. Aplikasi ini memungkinkan jamaah masjid untuk memberikan infaq secara
+                    digital melalui berbagai metode pembayaran, seperti transfer bank, e-wallet, atau QR code.
+                </p>
             </div>
         </div>
     </main>
