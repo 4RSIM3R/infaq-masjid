b44a0988fc7a6aca8fc361376e6d539637824a69
ilzam <ilzammulkhaq85@gmail.com>
2024-09-17 10:06:25 +0700

    fix: logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
                     <a href="https://flowbite.com" class="flex items-center justify-between mr-4 w-56">
                         <img src="{{ asset('white.png') }}" class="mr-3 w-12 h-12"  />
                     </a>
-                    <form action="#" method="GET" class="hidden md:block md:pl-2">
+                    {{-- <form action="#" method="GET" class="hidden md:block md:pl-2">
                         <label for="topbar-search" class="sr-only">Search</label>
                         <div class="relative md:w-96">
                             <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
@@ -69,7 +69,7 @@
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                 placeholder="Search Activity" />
                         </div>
-                    </form>
+                    </form> --}}
                 </div>
                 <div class="flex items-center lg:order-2">
                     <button type="button"
@@ -91,7 +91,7 @@
                         </div>
                         <ul class="py-1 text-gray-700 dark:text-gray-300" aria-labelledby="dropdown">
                             <li>
-                                <a href="{{ route('logout') }}"
+                                <a href="{{ route('logout.store') }}"
                                     class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                                     Sign out
                                 </a>
